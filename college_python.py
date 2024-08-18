d2eeb7037d84c7afb9205f1242f752af4db7f178
Akanksha Kumari <youakanksha@gmail.com>
2024-08-18 23:44:01 +0530

    college first class

--- college_python.py
+++ college_python.py
@@ -5,3 +5,17 @@ TEACHER
 DAY''')
 
 print(2+10)
+
+x="BO CDA"
+y=105
+print(x,y)
+type(x)
+
+m,n,o,p,q="Python","is","great","language","a"
+print(m,n,q,o,p)
+
+x=25
+y=5
+z=x+y
+print('z')
+print(z)
