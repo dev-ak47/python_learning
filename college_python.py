e800f506ac9d26da4edda42af9eb83c370893fd1
Akanksha Kumari <youakanksha@gmail.com>
2024-08-26 12:13:15 +0530

    removed some mistakes

--- college_python.py
+++ college_python.py
@@ -1,65 +1,67 @@
-# print ('HELLO WORLD!')
-# print ("HAPPY TEACHER'S DAY!")
-# print('''HAPPY
-# TEACHER 
-# DAY''')
-# # 
-# #
-# print(2+10)
-# #
-# #
-# x="BO CDA"
-# y=105
-# print(x,y)
-# type(x)
-# #
-# #
-# m,n,o,p,q="Python","is","great","language","a"
-# print(m,n,q,o,p)
-# #
-# #
-# x=25.5
-# y=5
-# z=x+y
-# print('z')
-# print(z)
-# print("Sum of",x,"and",y,"is",z)
-# print(type(x),type(y),type(z))
-# #
-# #
-# x=input("Enter a number: ")
-# print("You have entered",x)
-# #
-# #
-# x=25
-# y=4
-# z=x%y      # **double star is for power
-# print(z)
-# #
-# #
-# x=int(input("Enter first number: "))
-# y=int(input("Enter second number: "))
-# z=x+y
-# print("Sum of two numbers is",z)
-# #
-# #
-# 
-# x=['Ram',1,2,3,5,7,"Iksha"]
-# x.append('123')
-# print(x)
-# x.insert(1,"Sita")
-# print(x)
-# print(len(x))
-# x.remove(5)
-# x.remove(7)
-# print(x)
+print ('HELLO WORLD!')
+print ("HAPPY TEACHER'S DAY!")
+print('''HAPPY
+TEACHER 
+DAY''')
+##
+##
+print(2+10)
+##
+##
+x="BO CDA"
+y=105
+print(x,y)
+type(x)
+##
+##
+m,n,o,p,q="Python","is","great","language","a"
+print(m,n,q,o,p)
+##
+##
+x=25.5
+y=5
+z=x+y
+print('z')
+print(z)
+print("Sum of",x,"and",y,"is",z)
+print(type(x),type(y),type(z))
+##
+##
+x=input("Enter a number: ")
+print("You have entered",x)
+##
+##
+x=25
+y=4
+z=x%y      # **double star is for power
+print(z)
+##
+##
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+z=x+y
+print("Sum of two numbers is",z)
+##
+##
 
-# x=int(input("Enter a number:"))
-# if x%2==0:
-#     print("no. is even")
-# else:
-#     print("no is odd")
-
+x=['Ram',1,2,3,5,7,"Iksha"]
+x.append('123')
+print(x)
+x.insert(1,"Sita")
+print(x)
+print(len(x))
+x.remove(5)
+x.remove(7)
+print(x)
+##
+##
+x=int(input("Enter a number:"))
+if x%2==0:
+    print("no. is even")
+else:
+    print("no is odd")
+##
+##
 x=int(input("Enter:"))
 if x%3==0:
     print("x is divisible by 3")
