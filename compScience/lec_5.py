301f084fe1d67f64b9e05cf3c74e06be06d8a265
Akanksha Kumari <youakanksha@gmail.com>
2024-10-20 16:35:33 +0530

    Lec 06 wip

--- compScience/lec_5.py
+++ compScience/lec_5.py
@@ -2,57 +2,38 @@
 ##Assume that the solutions are real.
 ##We know that the solutions are given as x= (-b+sqrt(b^2-4ac))/2 and (-b-sqrt(b^2-4ac))/2
 
-# import math
+import math
 
-# def findRootsQE(a,b,c):
-#     d=math.sqrt(b**2-4*a*c)
-#     r1=(-b+d)/(2*a)
-#     r2=(-b-d)/(2*a)
-#     print("The roots of the QE is given as ",r1,"and",r2)
+def findRootsQE(a,b,c):
+    d=math.sqrt(b**2-4*a*c)
+    r1=(-b+d)/(2*a)
+    r2=(-b-d)/(2*a)
+    print("The roots of the QE is given as ",r1,"and",r2)
 
-# def main():
-#     a=eval(input("Enter coefficient a: "))
-#     b=eval(input("Enter coefficient b: "))
-#     c=eval(input("Enter coeffecient c: "))
-#     findRootsQE(a,b,c)
+def main():
+    a=eval(input("Enter coefficient a: "))
+    b=eval(input("Enter coefficient b: "))
+    c=eval(input("Enter coeffecient c: "))
+    findRootsQE(a,b,c)
 
-# main()
+main()
 
 ##Let us consider agai our celcius to fahrenheit temperature program
 
-# c=int(input("Enter the temperature in centigrade: "))
-# f=(9/5*c)+32
-# print("Temperature in fahrenheit is: ",f)
+c=int(input("Enter the temperature in centigrade: "))
+f=(9/5*c)+32
+print("Temperature in fahrenheit is: ",f)
 
-# if(f>=90):
-#     print("Heat warning")
-# else:
-#     print("Cold warning")
+if(f>=90):
+    print("Heat warning")
+else:
+    print("Cold warning")
 
 ##How can we enhance this program to print a suitable warning when the temperature is extreme(say over 90 degrees F, it deserves a heat 
 ##warning,and under 30 degree it deserves a cold warning).
 
 ##For the quadratic equation,check whether discriminant b^2-4ac > 0.If yes print the result,else print the result is complex and 
 ##then print result is same.
-
-# import math 
-# def findRootsQE(a,b,c):
-#     d=(b**2-4*a*c)
-#     if d>=0:
-#         r1=(-b+math.sqrt(d))/(2*a)
-#         r2=(-b-math.sqrt(d))/(2*a)
-#         print("The roots of the QE is given as ",r1,"and",r2)
-#     else:
-#         d1=math.sqrt(abs(d))
-#         r_real=-b/2*a
-#         r_complex=d1/2*a
-#         print("The roots of the QE is given as ",r_real,"+",r_complex,"i and",r_real,"-",r_complex,"i" )
-
-# def main():
-#     a=int(input("Enter coefficient a: "))
-#     b=int(input("Enter coefficient b: "))
-#     c=int(input("Enter coeffecient c: "))
-#     findRootsQE(a,b,c)
 
 import math 
 
---
+++ compScience/lec_6.py
@@ -0,0 +1,116 @@
+## Global and Local variable
+
+# x=15##Global
+
+# def func1(y,z):
+#     global x ##(I wrote this and now x=6 will become global)
+#     x=6##Local
+#     y=5
+#     z=6
+#     print("In func1: ",x,y,z)
+
+# def func2(y,z):
+#     y=7
+#     z=8
+#     print("In func2 before func1 call: ",x,y,z)
+#     func1(y,z)
+#     print("In func2 after func1 call",x,y,z)
+
+# func2(2,3)
+
+## PYTHON LAB-2
+## Ques-01
+
+# import math
+# import cmath ##For handling complex numbers
+
+# def root(a,b,c):
+#     d= (b**2)-4*a*c
+
+#     if d>0:
+#         r1 = (-b + math.sqrt(d)) / (2*a)
+#         r2 = (-b - math.sqrt(d)) / (2*a)
+#         print("The equation has two real and distinct roots.")
+#         print("Root1: ",r1)
+#         print("Root2: ",r2)
+#     elif d==0:
+#         print("The roots are real and equal.")
+#         root=-b/2*a
+#         print("Root: ",root)
+#     else:
+#         print("The roots are complex.")
+#         r1 = (-b + math.sqrt(d)) / (2*a)
+#         r2 = (-b - math.sqrt(d)) / (2*a)
+#         print("Root1: ",r1)
+#         print("Root2: ",r2)
+
+# ##Input from user
+# a=float(input("Enter coefficient a: "))
+# b=float(input("Enter coefficient b: "))
+# c=float(input("Enter coefficient c: "))
+
+# ##Ensure that a is not zero.
+# if a==0:
+#     print("This is not a quadratic equation,as a cannot be zero.")
+# else:
+#     root(a,b,c)
+
+## Ques-02
+# import math
+
+# def findCenterDist(x1,y1,x2,y2):
+#     D2=(x1-x2)**2+(y1-y2)**2
+#     return math.sqrt(D2)
+
+# def main():
+#     x1=eval(input("Enter x1: "))
+#     y1=eval(input("Enter y1: "))
+#     x2=eval(input("Enter x2: "))
+#     y2=eval(input("Enter y2: "))
+#     r1=eval(input("Enter radius of circle1: "))
+#     r2=eval(input("Enter radius of circle2: "))
+
+#     r=findCenterDist(x1,y1,x2,y2)
+#     if r==r1+r2:
+#         print("The circles touch externally.")
+#     elif r>r1+r2:
+#         print("The circles are completely separate.")
+#     else:
+#         print("The circles intersect.")
+
+# main() 
+
+## Ques-03
+# import math
+
+# def height(v,theta):
+#     g=9.8
+#     # Convert theta from degrees to radians
+#     theta_rad = math.radians(theta)
+#     h=((v**2*math.sin(theta_rad)**2)/(2*g))
+#     return h
+
+# def main():
+#     v=eval(input("Enter initial velocity(m/s): "))
+#     theta=eval(input("Enter angle of projection(degrees): "))
+#     h=height(v,theta)
+#     print("Maximun height reached by the projectile: ",h)
+
+# main()
+
+##Ques-04
+
+def complexdiv(a,b,c,d):
+    real=(a*c+b*d)/(c**2+d**2)
+    imag=(b*c-a*d)/(c**2+d**2)
+    print("The result is: ",real,"+",imag,"i")
+
+def main():
+    a=eval(input("Enter real part of first complex number: "))
+    b=eval(input("Enter complex part of first complex number: "))
+
+    c=eval(input("Enter real part of second complex number: "))
+    d=eval(input("Enter complex part of second complex number: "))
+    complexdiv(a,b,c,d)
+
+main()
